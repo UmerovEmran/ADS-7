6d5f5a1e4c4fe57495222cb53cc31a796c649793
UmerovEmran <99113996+UmerovEmran@users.noreply.github.com>
2022-04-30 19:46:56 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,75 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct Ltqueue {
+    T value;
+    Ltqueue* prev;
+    Ltqueue* next
+  };
+  Ltqueue* tail;
+  Ltqueue* head;
+  TPQueue::Ltqueue* creat(const T&);
+ public:
+  TPQueue():head(nullptr), tail(nullptr) {}
+  T pop();
+  void push(const T&);
+};
 };
 
 struct SYM {
   char ch;
   int prior;
 };
+
+template<typename T>
+T TPQueue<T>::pop() {
+  if (head && tail) {
+      Ltqueue* pass = head->next;
+    if (!head) {
+      tail = nullptr;
+    }
+    if (pass) {
+      pass->prev = nullptr;
+    }
+    T value = head->value;
+    delete head;
+    head = pass;
+    return value;
+  }
+}
+
+template < typename T >
+void TPQueue <T>::push(const T& value) {
+  Ltqueue* pass = head;
+  Ltqueue* mean = create(value);
+  for (bool i = false; i != true; ) {
+    if (pass && pass->value.prior >= value.prior) {
+        pass = pass->next;
+    } else {
+        i = true;
+      }
+  }
+  if (!pass && head) {
+    tail->next = mean;
+    mean->prev = tail;
+    tail = mean;
+  } else if (!pass && !head) {
+      head = tail = mean;
+  } else if (!pass->prev) {
+      head->prev = mean;
+      mean->next = head;
+      head = mean;
+  } else {
+      pass->prev->next = mean;
+      mean->prev = pass->prev;
+      mean->next = pass;
+      pass->prev = mean;
+  }
+}
 
 #endif  // INCLUDE_TPQUEUE_H_
